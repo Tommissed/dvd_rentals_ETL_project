4ec24a80de17e14fd9512ec3bd896d58d7b029e9
vinaqn <vina3nguyen@gmail.com>
2025-11-20 05:46:32 -0800

    created dim_address, dim_date, dim_staff

--- project_2_dvd_rentals/models/mart/dim_address_scd.sql
+++ project_2_dvd_rentals/models/mart/dim_address_scd.sql
@@ -9,10 +9,13 @@ select
     address,
     address2,
     district,
-    city_id,
+    city.city,
     postal_code,
+    country.country,
     phone,
-    dbt_valid_from as effective_start_date,
-    dbt_valid_to as effective_end_date,
+    cast(dbt_valid_from as date) as effective_start_date,
+    cast(dbt_valid_to as date) as effective_end_date,
     case when dbt_valid_to is null then true else false end as is_current
 from {{ ref('address_history') }}   
+left join {{ref('city')}} on city.city_id=address_history.city_id
+left join {{ref('country')}} on country.country_id=city.country_id
