7f4888b6579774261b67d16f6e92651da0a19ed5
Tommissed <telaurel@gmail.com>
2025-11-21 11:28:34 -0500

    customer dim table

---
+++ project_2_dvd_rentals/models/mart/dim_customer.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+--customer -> address -> city -> country
+--bring into one dim table to maintain a star schema
+
+select
+    c.customer_id,
+    c.store_id,
+    concat(c.first_name, ' ', c.last_name) as full_name,
+    c.first_name,
+    c.last_name,
+    c.email,
+    c.activebool,
+    a.address,
+    a.district,
+    a.postal_code,
+    ci.city,
+    co.country
+from {{ ref('customer') }} as c
+left join {{ ref('address')}} as a
+    on a.address_id=c.address_id
+left join {{ ref('city')}} as ci 
+    on ci.city_id=a.city_id
+left join {{ ref('country')}} as co 
+    on co.country_id=ci.country_id
