9fc391bf8ca7796ee0580202f2db32a72af6e36b
Tommissed <telaurel@gmail.com>
2025-11-21 12:51:22 -0500

    film dim table - created actor list column to prevent actor to film many to one relationship

--- project_2_dvd_rentals/models/mart/dim_film.sql
+++ project_2_dvd_rentals/models/mart/dim_film.sql
@@ -10,36 +10,40 @@
 -- to fix: one row should correspond to one film
 -- currently because of multiple actors per film,
 --  one film will have MANY rows, for as many actors there are.
+-- need to make an actor list so that for one movie,
+--  there will only be one row corresponding to its actors
+
+--returns a list of all actors in a movie
+with actor_list as (select 
+    f.film_id,
+    LISTAGG(concat(a.first_name, ' ', a.last_name), ', ') as actors
+from {{ ref('film')}} as f
+left join {{ ref('film_actor')}} as fa
+    on f.film_id=fa.film_id
+left join {{ ref('actor')}} as a
+    on fa.actor_id=a.actor_id)
 
 select
-    i.inventory_id,
-    i.store_id,
-    film.title,
-    film.description,
-    film.release_year,
+    f.film_id,
+    f.title,
+    al.actors,
+    f.description,
+    f.release_year,
     l.name as language,
-    film.rental_duration,
-    film.rental_rate,
-    film.length,
-    film.replacement_cost,
-    film.rating,
-    film.special_features,
-    film.fulltext,
-    a.first_name,
-    a.last_name,
-    concat(a.first_name, ' ', a.last_name) as actor_full_name,
+    f.rental_duration,
+    f.rental_rate,
+    f.length,
+    f.replacement_cost,
+    f.rating,
+    f.special_features,
     c.name as category
 
-from {{ ref('inventory') }} as i
-left join {{ ref('film')}} as film
-    on i.film_id=film.film_id
+from {{ ref('film')}} as f
 left join {{ ref('language')}} as l
-    on film.language_id=l.language_id
-left join {{ ref('film_actor')}} as fa
-    on film.film_id=fa.film_id
-left join {{ ref('actor')}} as a
-    on fa.actor_id=a.actor_id
+    on f.language_id=l.language_id
 left join {{ ref('film_category')}} as fc
-    on film.category_id=fc.category_id
+    on f.film_id=fc.film_id
 left join {{ ref('category')}} as c
     on fc.category_id=c.category_id
+left join actor_list as al
+    on f.film_id=al.film_id;
