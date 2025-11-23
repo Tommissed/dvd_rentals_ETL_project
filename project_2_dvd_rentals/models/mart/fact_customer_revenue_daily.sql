3a56474d993ca6c9aefc65888b0b96973e16723f
vinaqn <vina3nguyen@gmail.com>
2025-11-23 13:39:38 -0800

    altered fact_film_revenue to include customer_id

--- project_2_dvd_rentals/models/mart/fact_customer_revenue_daily.sql
+++
@@ -1,7 +0,0 @@
-select 
-p.customer_id,
-p.payment_date,
-sum(p.amount) as film_revenue
-from {{ref('payment')}} p
-group by p.customer_id, p.payment_date
-order by p.payment_date,p.customer_id
--- project_2_dvd_rentals/models/mart/fact_film_revenue_daily.sql
+++ project_2_dvd_rentals/models/mart/fact_film_revenue_daily.sql
@@ -1,11 +1,12 @@
 select 
 f.film_id,
 p.payment_date,
+p.customer_id,
 sum(p.amount) as film_revenue,
 count(r.rental_id) as rental_count 
 from {{ref('payment')}} p
 inner join {{ref('rental')}} r on r.rental_id=p.rental_id
 inner join {{ref('inventory')}} i on i.inventory_id=r.inventory_id
 inner join {{ref('film')}} f on f.film_id=i.film_id
-group by f.film_id,p.payment_date
+group by f.film_id,p.payment_date,p.customer_id
 order by p.payment_date,f.film_id
